c8c1e4a27fa9edbf5b9806760bfc45697261d86a
Disvster <mmcouto2001@gmail.com>
2025-04-22 01:48:18 +0100

    ft_printf VERY wip

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:09:24 by manmaria          #+#    #+#             */
-/*   Updated: 2025/04/21 23:38:54 by manmaria         ###   ########.fr       */
+/*   Updated: 2025/04/22 01:47:53 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,38 @@ char	*ft_strchr(const char *s, int c)
 	}
 	if (s[i] == (char)c)
 		return ((char *)s + i);
-	return (NULL);
+	return ((char *)s);
 }
 
-void	ft_percent_scanner(const char *s, char *)
+void	ft_percent_scanner(const char *s, va_list valet)
 {
+	int	i;
 	
+	i = 0;
+	while (s[i]) 
+	{
+		if (s[i] == 'c')
+			ft_putchar_fd(va_arg(valet, char), 1);
+		else if (s[i] == 'i')
+			ft_putnbr_fd(va_arg(valet, int), 1);
+		i++
+
+	}
 }
+
 int	ft_printf(const char *s, ...)
 {
-	va_list	varg;
+	va_list	valet;
 	int		i;
 
-	va_start(varg, s);
+	va_start(valet, s);
 	i = 0;
 	while (s[i])
 	{
-		if (a1
+		 if (s[i] == '%')
+			ft_percent_scanner(&s[i]);
 	}
-	va_end(varg);
+	va_end(valet);
 	return (0);
 }
 /*
@@ -54,7 +67,7 @@ int	ft_printf(const char *s, ...)
 		{
 			if (s[i] == '%' && s[i + 1] == 's')
 			{
-				ft_putstr_fd(va_arg(varg, char *), 1);
+				ft_putstr_fd(va_arg(valet, char *), 1);
 				i += 2;
 			}
 			ft_putchar_fd(s[i], 1);
@@ -63,7 +76,7 @@ int	ft_printf(const char *s, ...)
 	}
 	else if (*s)
 		ft_putstr_fd((char *)s, 1);
-	va_end(varg);
+	va_end(valet);
 */
 int	main(int ac, char **av)
 {
